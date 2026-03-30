fabc6fb9f9ef91676941cd8a993388ed2f66b5f4
Martin Høgh <mh@mapcentia.com>
2026-03-30 16:39:03 +0200

    Add: Load environment variables dynamically in Vite config to support mode-specific configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
-  plugins: [react()],
-  server: { port: 4000 },
-  resolve: {
-    alias: {
-      '@centia-io/sdk': path.resolve(__dirname, 'node_modules/@centia-io/sdk/dist/centia-io-sdk.js'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [react()],
+    server: { port: 4000 },
+    resolve: {
+      alias: {
+        '@centia-io/sdk': path.resolve(__dirname, 'node_modules/@centia-io/sdk/dist/centia-io-sdk.js'),
+      },
     },
-  },
+  };
 });
